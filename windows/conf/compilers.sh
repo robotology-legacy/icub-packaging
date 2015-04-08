3778b4035e516f58e38fa4d38c6cf7b9ee87ae53
Matteo Brunettini <matteo.brunettini@iit.it>
2015-04-08 09:00:00 +0200

    Update compilers in icub package creation

--- windows/conf/compilers.sh
+++ windows/conf/compilers.sh
@@ -1,17 +1,22 @@
 compilers="v11 v10 v9" # v11 v10 v9 v8"  list of compilers
+compiler_v12_family="msvc"
+compiler_v12_loader="E:/VisualStudio/VS12/VC/vcvarsall.bat"
+compiler_v12_variants="x86 x86_amd64"
 compiler_v11_family="msvc"
 compiler_v11_loader="F:/VS1/VC/vcvarsall.bat"
 compiler_v11_variants="x86" #x86 64-bit, etc
 compiler_v10_family="msvc"
 compiler_v10_loader="F:/VS10/VC/vcvarsall.bat"
 compiler_v10_variants="x86" #x86 64-bit, etc
-compiler_v9_family="msvc"
-compiler_v9_loader="F:/VS9/VC/vcvarsall.bat"
-compiler_v9_variants="x86"
-compiler_v8_family="msvc"
-compiler_v8_loader="F:/VS8/VC/vcvarsall.bat"
-compiler_v8_variants="x86"
-compiler_mingw_variants="x86"
+# V9 in not supported anymore
+#compiler_v9_family="msvc"
+#compiler_v9_loader="F:/VS9/VC/vcvarsall.bat"
+#compiler_v9_variants="x86"
+# V8 in not supported anymore
+#compiler_v8_family="msvc"
+#compiler_v8_loader="F:/VS8/VC/vcvarsall.bat"
+#compiler_v8_variants="x86"
+#compiler_mingw_variants="x86"
 #compiler_mingw_family="mingw"
 # compiler_mingw_loader="F:/MinGW"
 
